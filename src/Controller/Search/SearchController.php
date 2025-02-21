964bc8f0accc5ca43c8b6cbedf0e5cfda6ce4ded
Alban P <albanpirraud@hotmail.fr>
2025-02-21 20:01:46 +0100

    Mise à jour de la barre de recherche

--- src/Controller/Search/SearchController.php
+++ src/Controller/Search/SearchController.php
@@ -17,52 +17,39 @@ final class SearchController extends AbstractController
     #[Route('app/search', name: 'app_search')]
     public function searchData(Request $request): Response
     {
-
-        $form = $this->createForm(SearchInputType::class);
-        $form->handleRequest($request);
-
         $results = [
             'users' => [],
             'podcasts' => [],
         ];
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $searchValue = $form->get('query')->getData();
+        $searchValue = $request->get('query');
 
-            if ($searchValue) {
+        if ($searchValue) {
 
-                $users = $this->entityManagerInterface->createQueryBuilder()
-                    ->select('u')
-                    ->from('App\Entity\User', 'u')
-                    ->where('LOWER(u.username) LIKE LOWER(:term)')
-                    ->setParameter('term', '%' . $searchValue . '%')
-                    ->getQuery()
-                    ->getResult();
+            $users = $this->entityManagerInterface->createQueryBuilder()
+                ->select('u')
+                ->from('App\Entity\User', 'u')
+                ->where('LOWER(u.username) LIKE LOWER(:term)')
+                ->setParameter('term', '%' . $searchValue . '%')
+                ->getQuery()
+                ->getResult();
 
-                $podcasts = $this->entityManagerInterface->createQueryBuilder()
-                    ->select('p')
-                    ->from('App\Entity\Podcast', 'p')
-                    ->where('LOWER(p.name) LIKE LOWER(:term)')
-                    ->setParameter('term', '%' . $searchValue . '%')
-                    ->getQuery()
-                    ->getResult();
+            $podcasts = $this->entityManagerInterface->createQueryBuilder()
+                ->select('p')
+                ->from('App\Entity\Podcast', 'p')
+                ->where('LOWER(p.name) LIKE LOWER(:term)')
+                ->setParameter('term', '%' . $searchValue . '%')
+                ->getQuery()
+                ->getResult();
 
-                $categories = $this->entityManagerInterface->createQueryBuilder()
-                    ->select('c')
-                    ->from('App\Entity\Category', 'c')
-                    ->where('LOWER(c.name) LIKE LOWER(:term)')
-                    ->setParameter('term', '%' . $searchValue . '%')
-                    ->getQuery()
-                    ->getResult();
-
-                $results = [
-                    'users' => $users,
-                    'podcasts' => $podcasts
-                ];
-            }
+            $results = [
+                'users' => $users,
+                'podcasts' => $podcasts
+            ];
         }
+
         return $this->render('search/index.html.twig', [
-            'form' => $form,
+            'searchValue' => $searchValue,
             'results' => $results
         ]);
     }

e5b8a94a95f5924c46e4719f6f49f705340eafac
Alban P <albanpirraud@hotmail.fr>
2025-06-02 17:16:41 +0200

    Changements de quelques styles

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Account;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/app/account', name: 'app_account')]
 final class AccountController extends AbstractController
 {
-    #[Route('/app/account', name: 'app_account')]
+    #[Route('/', name: '.get', methods: ['GET'])]
     public function loadAccount(): Response
     {
 
@@ -17,5 +19,14 @@ final class AccountController extends AbstractController
         return $this->render('account/index.html.twig', [
             'user' => $user
         ]);
+    }
+
+    #[Route('/delete', name: '.delete')]
+    public function deleteAccount(EntityManagerInterface $entityManagerInterface): Response
+    {
+        $user = $this->getUser();
+        $entityManagerInterface->remove($user);
+        $this->addFlash('success', 'Votre compte a bien été supprimé.');
+        return $this->redirectToRoute('app_login');
     }
 }
